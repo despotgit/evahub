c1b09a618162ed8e5719cd6758594e50da9db508
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2022-07-16 15:59:09 +0200

    Designing.

--- backend/db_user_reports_broker.py
+++ backend/db_user_reports_broker.py
@@ -19,19 +19,19 @@ def getDbUserReports(username):
             "reportId": 1,
             "reportUserId": 1,
             "reportContent": "aaa",
-            "reportName": "first",
+            "reportName": "first report",
         },
         {
             "reportId": 2,
             "reportUserId": 1,
             "reportContent": "bbb",
-            "reportName": "second",
+            "reportName": "second report",
         },
         {
             "reportId": 3,
             "reportUserId": 1,
             "reportContent": "ccc",
-            "reportName": "third",
+            "reportName": "third report",
         },
     ]
 
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 import { ReportsComponent } from "./reports/reports.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'reports', component: ReportsComponent, canActivate: [AuthGuard] }
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "login", component: LoginComponent },
+    { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
+    { path: "reports", component: ReportsComponent, canActivate: [AuthGuard] },
+    { path: "**", component: LoginComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
