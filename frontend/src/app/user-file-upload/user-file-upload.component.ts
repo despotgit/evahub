405851b19777c2aba6270ea051f09cd0cd696aa7
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2022-08-06 18:06:27 +0200

    Throw on some stuff for upload.

--- frontend/src/app/user-file-upload/user-file-upload.component.ts
+++ frontend/src/app/user-file-upload/user-file-upload.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, Input } from "@angular/core";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-user-file-upload',
-  templateUrl: './user-file-upload.component.html',
-  styleUrls: ['./user-file-upload.component.scss']
+    selector: "evahub-user-file-upload",
+    templateUrl: "user-file-upload.component.html",
+    styleUrls: ["user-file-upload.component.scss"]
 })
-export class UserFileUploadComponent implements OnInit {
+export class UserFileUploadComponent {
+    @Input()
+    requiredFileType: string;
 
-  constructor() { }
+    fileName = "";
+    uploadProgress: number;
+    uploadSub: Subscription;
 
-  ngOnInit(): void {
-  }
+    constructor(private http: HttpClient) {}
 
+    onFileSelected(event) {
+        const file: File = event.target.files[0];
+
+        if (file) {
+            this.fileName = file.name;
+            const formData = new FormData();
+            formData.append("thumbnail", file);
+
+            const upload$ = this.http
+                .post("/api/thumbnail-upload", formData, {
+                    reportProgress: true,
+                    observe: "events"
+                })
+                .pipe(finalize(() => this.reset()));
+
+            this.uploadSub = upload$.subscribe(event => {
+                if (event.type == HttpEventType.UploadProgress) {
+                    this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+                }
+            });
+        }
+    }
+
+    cancelUpload() {
+        this.uploadSub.unsubscribe();
+        this.reset();
+    }
+
+    reset() {
+        this.uploadProgress = null;
+        this.uploadSub = null;
+    }
 }
