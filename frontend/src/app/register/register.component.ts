0ce3f079e5d7a4b834fcd87b06a787e1b459bac0
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2022-08-22 18:20:40 +0100

    Generic commit message.

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,84 +1,82 @@
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
 import {
-    Component,
-    OnInit,
-    AfterViewChecked,
-    ChangeDetectionStrategy,
-    ChangeDetectorRef
-} from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
-import { AuthenticationService } from "../services/authentication.service";
-import { PageIndex } from "../common/constants";
+    combineLatest,
+    debounceTime,
+    distinctUntilChanged,
+    map,
+    mergeMap,
+    Observable,
+    tap
+} from "rxjs";
+import { FormBuilder, FormControl } from "@angular/forms";
 import { ApplicationStateStoreService } from "../services/application-state-store.service";
+import { PageIndex } from "../common/constants";
 
 @Component({
     selector: "app-register",
     templateUrl: "./register.component.html",
-    styleUrls: ["./register.component.scss"]
+    styleUrls: ["./register.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RegisterComponent implements OnInit, AfterViewChecked {
-    loginForm: FormGroup;
-    submitted = false;
-    loading = false;
-    returnUrl: string = "";
-    error = "";
+export class RegisterComponent implements OnInit {
+    step1FormGroup: any;
+    step2FormGroup: any;
+
+    username$: Observable<string> = this.store.username$;
+    firstLastName$: Observable<string> = this.store.firstLastName$.pipe(
+        tap(newName => {
+            //console.log("CHECKPOINT 2");
+            this.step1FormGroup.get("firstLastNameFormControl").setValue(newName);
+        })
+    );
+    address$: Observable<string> = this.store.address$.pipe(
+        tap(address => {
+            this.step2FormGroup.get("addressFormControl").setValue(address);
+        })
+    );
 
     constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
         private router: Router,
-        private authenticationService: AuthenticationService,
         private store: ApplicationStateStoreService,
-        private cd: ChangeDetectorRef
+        private formBuilder: FormBuilder
     ) {
+        this.step1FormGroup = this.formBuilder.group({
+            firstLastNameFormControl: new FormControl("")
+        });
+
+        this.step2FormGroup = this.formBuilder.group({
+            addressFormControl: new FormControl("")
+        });
+
         this.ngOnInit();
     }
 
-    ngOnInit() {
-        //console.log("in ngoninit in login component");
-        this.loginForm = this.formBuilder.group({
-            username: ["", Validators.required],
-            password: ["", Validators.required]
-        });
+    ngOnInit(): void {
+        const firstLastNameEvents: Observable<any> = this.step1FormGroup.get(
+            "firstLastNameFormControl"
+        ).valueChanges;
+        const addressEvents: Observable<any> =
+            this.step2FormGroup.get("addressFormControl").valueChanges;
 
+        combineLatest([firstLastNameEvents, addressEvents])
+            .pipe(
+                debounceTime(300),
+                distinctUntilChanged(),
+                map(([firstLastName, address]) => {
+                    this.store.updateFirstLastName(firstLastName);
+                    this.store.updateAddress(address);
+                })
+            )
+            .subscribe();
         this.store.updateCurrentPageIndex(PageIndex.REGISTER_PAGE);
     }
 
-    ngAfterViewChecked() {
-        this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/documents/log";
+    doneStepper() {
+        //console.log('v is:', this.firstFormGroup.value);
     }
 
-    get f() {
-        return this.loginForm.controls;
-    }
-
-    // On submitting the username and password
-    onSubmit() {
-        this.submitted = true;
-
-        // stop if form is invalid
-        if (this.loginForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-
-        this.authenticationService
-            .login(this.f["username"].value, this.f["password"].value)
-            .subscribe(data => {
-                console.log("data is:", data);
-                if (data.authenticated) {
-                    this.router.navigate([this.returnUrl]);
-                } else {
-                    console.log("just before Wrong cred entered");
-                    this.error = data.message;
-                    console.log("error is:", this.error);
-                    this.cd.markForCheck();
-                }
-                this.loading = false;
-                return;
-            });
-
-        //loginSubscription.unsubscribe();
+    onChangeFirstLastName($event: any) {
+        //console.log("event is:", $event);
     }
 }
