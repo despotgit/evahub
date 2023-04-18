ccaf98c94ea91f3ef95aed3c6ef25033993ca5c6
jsachau <jsachau@AirvonjM1sachau.fritz.box>
2023-04-18 18:26:43 +0200

    these are testfiles for the components

--- backend/db_config.py
+++ backend/db_config.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = ""
 app.config["MYSQL_DATABASE_DB"] = "evahub"
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 
