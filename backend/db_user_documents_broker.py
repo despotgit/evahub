882e009cc04439d0ff5bced6cc150408448708fb
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2023-04-24 18:53:07 +0200

    Add centralized documents broker for all documents.

---
+++ backend/db_user_documents_broker.py
@@ -0,0 +1,93 @@
+import time
+from common import getUserDocumentsDir
+
+from db_config import getDb
+from db import executeCustomQuery
+
+db = getDb()
+
+
+def addDbUserDocument(username, documentType):
+    results = executeCustomQuery("insert into " + documentType + "s values()")
+    return
+
+
+def getDbUserDocument(username, documentId):
+    return
+
+
+def getUploadedUserDocuments(username, documentType):
+    tableName = documentType + "s"
+    idField = documentType + "_id"
+    nameField = documentType + "_name"
+    filenameField = documentType + "_filename"
+
+    results = executeCustomQuery(
+        "select "
+        + idField
+        + ", "
+        + nameField
+        + ", "
+        + filenameField
+        + " from "
+        + tableName
+        + " where username = '"
+        + str(username)
+        + "'"
+    )
+
+    toReturn = []
+    for r in results:
+        dir = getUserDocumentsDir("log", username)
+        filePath = dir + "/" + r[2]
+
+        content = ""
+
+        with open(filePath, "rb") as f:
+            lines = f.readlines()
+
+            for line in lines:
+                content = content + "\n" + str(line)
+                # print(line)
+
+        # c = str(content)
+        c = content
+        # print("content is:!!!!!")
+        # print(c)
+        toReturn.append(
+            {
+                documentType + "Id": r[0],
+                documentType + "Name": r[1],
+                documentType + "Content": c,
+            }
+        )
+
+    # print(toReturn)
+
+    return toReturn
+
+
+def deleteAllDbUserDocuments(username, documentType):
+    return
+
+
+def deleteUserDocument(username, documentType, id):
+    tableName = documentType + "s"
+    idField = documentType + "_id"
+
+    results = executeCustomQuery(
+        "delete from "
+        + tableName
+        + " where username = "
+        + str(username)
+        + " and "
+        + idField
+        + " = "
+        + id
+    )
+
+    return results
+
+
+def getLogContent(logLocation):
+    return ""
