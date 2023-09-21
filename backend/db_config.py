0af9840f69a95bfeafd740edf91e04b59c54894d
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2023-09-21 11:10:51 +0200

    Add db_config.py to git (remove it from gitignore).

---
+++ backend/db_config.py
@@ -0,0 +1,27 @@
+from flaskext.mysql import MySQL
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = ""  # juergen's setup is empty password
+app.config["MYSQL_DATABASE_DB"] = "evahub"
+app.config["MYSQL_DATABASE_HOST"] = "localhost"
+
+
+def getDb():
+    mysql = MySQL(
+        app,
+        prefix="",
+        host=app.config["MYSQL_DATABASE_HOST"],
+        user=app.config["MYSQL_DATABASE_USER"],
+        password=app.config["MYSQL_DATABASE_PASSWORD"],
+        db=app.config["MYSQL_DATABASE_DB"],
+        autocommit=True,
+    )
+    connection = mysql.connect()
+    return connection
+
+
+def fromDbConfig():
+    print("yeah from the db_config")
