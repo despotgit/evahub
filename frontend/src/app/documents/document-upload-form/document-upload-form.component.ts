80a07d71073c1f161b007feaf8773a27fad12986
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2023-09-25 22:32:37 +0200

    Refactor the new upooad component to use the old code instead.

--- frontend/src/app/documents/document-upload-form/document-upload-form.component.ts
+++ frontend/src/app/documents/document-upload-form/document-upload-form.component.ts
@@ -1,38 +1,83 @@
-import { HttpEventType } from "@angular/common/http";
-import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from "@angular/core";
-import { Observable, Subscription } from "rxjs";
+import { HttpClient, HttpEventType } from "@angular/common/http";
+import { ChangeDetectorRef, Component, Input, OnDestroy } from "@angular/core";
+import { Subscription, switchMap, map, Subject, Observable } from "rxjs";
+import { combineLatestWith, finalize } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+import { ApplicationStateStoreService } from "../../store/application-state-store";
 
 @Component({
-    selector: "app-document-upload-form",
-    templateUrl: "./document-upload-form.component.html",
-    styleUrls: ["./document-upload-form.component.scss"]
+    selector: "app-document-upload-form-component",
+    templateUrl: "document-upload-form.component.html",
+    styleUrls: ["document-upload-form.component.scss"]
 })
-export class DocumentUploadFormComponent {
+export class DocumentUploadFormComponent implements OnDestroy {
     @Input()
     requiredFileType: string = "png";
 
-    @Output()
-    emitFileSelected: EventEmitter<any> = new EventEmitter();
-
-    @Output()
-    emitUploadInitiated: EventEmitter<any> = new EventEmitter();
-
-    @Input()
-    uploadProgress: number;
-
-    @Output()
-    emitResetUpload: EventEmitter<any> = new EventEmitter();
-
     fileName = "";
+    uploadProgress: number;
+    uploadObs$: Observable<any>;
+    uploadSub$: Subscription;
 
-    constructor() {}
+    username$ = this.store.username$;
+
+    constructor(
+        private http: HttpClient,
+        private store: ApplicationStateStoreService,
+        private cd: ChangeDetectorRef
+    ) {
+        //this.store.updateSidenavMenuItems([]);
+        //this.store.updateIsSidenavOpened(false);
+    }
 
     onFileSelected(event) {
-        //const file: File = event.target.files[0];
-        this.emitFileSelected.emit(event);
+        const file: File = event.target.files[0];
+
+        if (file) {
+            this.fileName = file.name;
+            const formData = new FormData();
+            formData.append("file", file);
+
+            this.uploadObs$ = this.username$.pipe(
+                switchMap(username => {
+                    let url = `${environment.baseApiBackendUrl}/rest/put/document/document-type/log/username/${username}`;
+
+                    return this.http.put(url, formData, {
+                        reportProgress: true,
+                        observe: "events"
+                    });
+                }),
+                finalize(() => {
+                    console.log("step 3, in finalize");
+                    this.resetUpload();
+                })
+            );
+        }
     }
 
     onUploadInitiated() {
-        this.emitUploadInitiated.emit();
+        console.log();
+
+        this.uploadSub$ = this.uploadObs$.subscribe(event => {
+            if (event.type == HttpEventType.UploadProgress) {
+                console.log("UPLOAD PROGRESS, event is:", event);
+                const newProgress = Math.round(100 * (event.loaded / event.total));
+                this.uploadProgress = newProgress;
+                this.cd.markForCheck();
+            }
+        });
+    }
+
+    resetUpload() {
+        if (this.uploadSub$) {
+            this.uploadSub$.unsubscribe();
+        }
+
+        this.uploadProgress = null;
+        this.uploadSub$ = null;
+    }
+
+    ngOnDestroy() {
+        this.resetUpload();
     }
 }
