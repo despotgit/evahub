00fe9b72becf6c57fd80dfcc6bb59a59b4819df0
Despotovic Vladimir <despotovic_vladimir@yahoo.ie>
2024-11-16 22:46:57 +0100

    Add stuff, restarting in version 2.0

--- backend/db_config.py
+++ backend/db_config.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = ""  # juergen's setup is empty password
+app.config["MYSQL_DATABASE_PASSWORD"] = "root"  # juergen's setup is empty password
 app.config["MYSQL_DATABASE_DB"] = "evahub"
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 
